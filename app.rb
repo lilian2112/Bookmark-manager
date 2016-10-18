e940c6aed3dd4ebc80e21736464eef6f2012b507
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-10-18 11:07:41 +0100

    added link creation facility

--- app.rb
+++ app.rb
@@ -3,8 +3,21 @@ require './models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    redirect '/links'
+  end
+
   get '/links' do
     erb :links
+  end
+
+  get '/links/new' do
+    erb :new_link
+  end
+
+  post '/links' do
+  	Link.create(title: params[:title], url: params[:url])
+  	redirect '/'
   end
 
   run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,9 +1,23 @@
 feature 'list added links' do
   scenario 'added link appears on homepage' do
     Link.create(title: "Facebook", url: 'http://www.facebook.com')
-    visit '/links'
+    visit '/'
     within 'ul#links' do
     expect(page).to have_content "Facebook"
    end
+	end
 end
+
+
+feature 'creating links' do
+  scenario 'add a new link' do
+    visit '/'
+    click_button 'Add New Link'
+    fill_in :title, with: 'Facebook'
+    fill_in :url, with: 'http://www.facebook.com'
+    click_button 'Submit'
+    within 'ul#links' do
+    expect(page).to have_content "Facebook"
+   end
+	end
 end
