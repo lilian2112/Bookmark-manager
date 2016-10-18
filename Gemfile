b43c8873cfba51667b303569eab9670bb4c52cff
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-18 10:00:48 +0100

    up to 10 of challenge

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+ruby '2.3.0'
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+gem 'capybara'
---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,11 @@
+
+
+feature 'list added links' do
+  scenario 'added link appears on homepage' do
+    Link.create(title: "Facebook", url: 'http://www.makersacademy.com')
+    visit '/links'
+    within 'ul#links' do
+    expect(page).to have_content "Facebook"
+  end
+end
+end
