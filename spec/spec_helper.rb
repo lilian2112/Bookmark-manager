8b4e8869f8dbad426cdbb61a3538b3965ff2855e
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-18 13:41:18 +0100

    add

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV['RACK_ENV'] = 'test'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -101,7 +102,6 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 
-ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
@@ -114,11 +114,11 @@ Capybara.app = BookmarkManager
 
 end
 
-RSpec.configure do |config|
-  DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
-  DataMapper.finalize
-  DataMapper.auto_upgrade!
-end
+# RSpec.configure do |config|
+#   DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
+#   DataMapper.finalize
+#   DataMapper.auto_upgrade!
+# end
 
 require 'database_cleaner'
 
@@ -140,5 +140,3 @@ RSpec.configure do |config|
   end
 
 end
-
-
